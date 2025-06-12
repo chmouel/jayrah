fff014ede716c5a88f766f1c660fb00e6773c759
Chmouel Boudjnah <chmouel@chmouel.com>
2025-06-12 18:23:24 +0200

    remove unused function

--- jayrah/ui/tui/css.py
+++
@@ -1,9 +0,0 @@
-"""CSS loader for Textual UI components"""
-
-import pathlib
-
-
-def get_css_path(filename):
-    """Get the path to a CSS file in the ui package."""
-    pkg_path = pathlib.Path(__file__).parent.resolve()
-    return pkg_path / filename
